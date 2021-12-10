2a977324d813768796862bc991cd8160ccdeb546
Nicola Marangoni <n.marangoni@donboscosandona.it>
2021-12-10 11:43:29 +0000

    application

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,13 @@ class HomeController < ApplicationController
         # ma richiama la corrispondente
         # vista index
     end
-
+    
     def chi_siamo
+    end
+    def contatti
+    end
+    def prodotti
+    end
+    def servizi
     end
 end
